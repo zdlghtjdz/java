cceb3385f3c58ce4e6c539492727616a80b2bf77
zdlghtjdz <43461948+zdlghtjdz@users.noreply.github.com>
2022-12-10 23:21:08 +0900

    Update [프로그래머스]푸드파이트.java
    reverse 함수 로직 수정

--- [프로그래머스]푸드파이트.java
+++ [프로그래머스]푸드파이트.java
@@ -3,14 +3,14 @@ class Solution {
         String answer = ""; // 물은 항상 존재
     
         StringBuilder sb = new StringBuilder(""); 
-        for(int i = food.length-1; i > 0 ; i--) {
+        for(int i = 0; i < food.length ; i++) {
             
             for(int j = 0; j < (food[i] / 2); j++) {
                 sb.append(i);
             }
         }
         
-        answer = sb.reverse().toString() + "0" + sb.reverse().toString();
+        answer = sb.toString() + "0" + sb.reverse().toString();
         
         return answer;
     }
